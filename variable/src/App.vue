21333d4cb018b1890638c8ae7277303e64cc1b0a
GerardoNR <22620048@tlaxiaco.tecnm.mx>
2025-03-26 14:04:24 -0600

    ordene los archivos

--- variable/src/App.vue
+++ variable/src/App.vue
@@ -1,5 +1,5 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue';
+import HelloWorld from './components/horario/HelloWorld.vue';
 import padre from './components/props/padre.vue';
 </script>
 
